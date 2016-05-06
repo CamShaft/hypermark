9fe4b2f199fb78862511e60ea439c35f18c0e436
Cameron Bytheway <bytheway.cameron@gmail.com>
2016-05-05 18:22:53 -0600

    make Router.resolve_resource optional

--- lib/mazurka/router.ex
+++ lib/mazurka/router.ex
@@ -19,5 +19,12 @@ defimpl Mazurka.Router, for: Atom do
 
   def resolve_resource(router, module, source, conn) do
     router.resolve_resource(module, source, conn)
+  rescue e in UndefinedFunctionError ->
+    case e do
+      %{module: ^router, function: :resolve_resource} ->
+        module
+      _ ->
+        reraise e, System.stacktrace
+    end
   end
 end
